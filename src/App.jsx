c244a296bb0211b0ad33244b26f4c3099ef8a0d2
Ashadul Alam Fardin <mdfardin6118@gmail.com>
2026-07-23 18:48:27 +0600

    chore: bump version to 1.0.1 for public repo test

--- src/App.jsx
+++ src/App.jsx
@@ -562,7 +562,7 @@ function App() {
   const [pinInput, setPinInput] = React.useState('');
   const [pinError, setPinError] = React.useState('');
   const [updateAvailable, setUpdateAvailable] = React.useState(false);
-  const currentVersion = '1.0.0';
+  const currentVersion = '1.0.1';
 
   React.useEffect(() => {
     // Hide the splash screen smoothly once the app is ready
