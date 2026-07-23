da8c55725ac1c50bcc7de42fb92922a5cb462ab7
Ashadul Alam Fardin <mdfardin6118@gmail.com>
2026-07-23 16:41:41 +0600

    chore: setup production environment variables

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAemNiOsk0-GRkhJPXQfTVzKdIhCvabmtM",
-  authDomain: "studyhub-d6422.firebaseapp.com",
-  projectId: "studyhub-d6422",
-  storageBucket: "studyhub-d6422.firebasestorage.app",
-  messagingSenderId: "1018023321139",
-  appId: "1:1018023321139:web:06165a45635b6f6ef6cd46"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
